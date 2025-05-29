3a23a5551d4ff2a04a57037bca8bcb2ff001f19e
Peter de Kraker <peterdekraker@umito.nl>
2025-05-29 18:05:22 +0200

    Fix bug

--- src/station.rs
+++ src/station.rs
@@ -94,7 +94,7 @@ impl TryFrom<Attrs<'_, Nl80211Attr>> for Station {
                             res.vht_mcs = vht_mcs.get_payload_as().ok();
                         }
                         if let Some(he_mcs) = rate_info.get_attribute(Nl80211RateInfo::RateInfoHeMcs) {
-                            res.ht_mcs = he_mcs.get_payload_as().ok();
+                            res.he_mcs = he_mcs.get_payload_as().ok();
                         }
                         if let Some(eht_mcs) = rate_info.get_attribute(Nl80211RateInfo::RateInfoEhtMcs) {
                             res.eht_mcs = eht_mcs.get_payload_as().ok();
